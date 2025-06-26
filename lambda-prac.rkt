c6a0ead2308532d828c6cef9b61e8bf52d50352a
Adkhamjon Khamidov <adkhamjonkhamidov@gmail.com>
2025-06-26 17:51:54 -0400

    curry

--- lambda-prac.rkt
+++ lambda-prac.rkt
@@ -2,3 +2,4 @@
 (define my-list '(1 2 3))
 (define (matrix-mult-scallar list-numbers scallar) 
   (for-each (lambda(x) (display (* x scallar) )) list-numbers))
+(define (curry-calc mult-one mult-two) ((lambda (x) ( (lambda (y) (* x y)) mult-two) ) mult-one))
