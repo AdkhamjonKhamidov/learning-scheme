cdecac1b1dc0c96d309b214a560b3e400397d2a0
Adkhamjon Khamidov <adkhamjonkhamidov@gmail.com>
2025-06-27 22:12:37 -0400

    data-types practice

---
+++ data-types.rkt
@@ -0,0 +1,5 @@
+#lang racket
+;Atoms (num, bools, char, symbols - var and const (literals))
+;Lists (pairs => head and tail)
+(cons 1 cons (2 3)) ; construct list - underlying structure is linked list in lisp
+
