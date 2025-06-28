910eb00d98f2c66113a4cf3eda67f30c8eddc8f4
Adkhamjon Khamidov <adkhamjonkhamidov@gmail.com>
2025-06-27 22:13:40 -0400

    list

--- data-types.rkt
+++ data-types.rkt
@@ -2,4 +2,4 @@
 ;Atoms (num, bools, char, symbols - var and const (literals))
 ;Lists (pairs => head and tail)
 (cons 1 cons (2 3)) ; construct list - underlying structure is linked list in lisp
-
+(list 1 2 3)
